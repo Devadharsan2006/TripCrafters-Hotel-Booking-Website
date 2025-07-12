aaa364ae604150044c2725d46b580c08964c9de3
Devadharsan2006 <133222609+Devadharsan2006@users.noreply.github.com>
2025-07-12 13:55:49 +0530

    Update PlaceImg.jsx

--- frontend/src/PlaceImg.jsx
+++ frontend/src/PlaceImg.jsx
@@ -5,6 +5,6 @@ export default function PlaceImg({place,index=0,classname=null})
     if(!classname)
         classname="object-cover"
     return(
-        <img className='object-cover'src={'http://localhost:4000/uploads/'+place.photos[index]}></img>
+        <img className='object-cover'src={'https://tripcrafters-hotel-booking-website-2.onrender.com/uploads/'+place.photos[index]}></img>
     )
 }
